fba5b5b8772109e081dc0de43c167b3960f53d43
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-07 03:46:25 +0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ const form = document.getElementById("formLogin")
          async function hendler(event){
         event.preventDefault();
         console.log(event.target.email.value);
-		 const f = await fetch("https://tatev5.github.io/BestChoiceNode/login", {
+		 const f = await fetch("https://tatev5.github.io/BestChoiceNode/users/login", {
             method: "POST",
 			  headers: {
 				'Accept': 'application/json, text/plain, */*',
