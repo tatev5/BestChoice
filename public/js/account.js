8a35b4dd5b2dcd610c62ccd7fbf1030cb6bf4be1
tatev5 <57810059+tatev5@users.noreply.github.com>
2021-02-08 17:42:17 +0400

    Update account.js

--- public/js/account.js
+++ public/js/account.js
@@ -1,7 +1,7 @@
 const navBar = document.getElementById("navBar")
 
     async function hendler(){
-        const f = await fetch("https://tatev5.github.io/BestChoice/auth", {
+        const f = await fetch("https://bestchoicenode.herokuapp.com/users/auth", {
         method: "POST",
                 headers: {
             'Accept': 'application/json, text/plain, */*',
